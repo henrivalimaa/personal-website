c5304beb2f5e1e75ef2e3e96646335af1d368267
henrivalimaa <valimaa.henri@gmail.com>
2018-10-02 19:08:20 +0300

    Portfolio view update

--- src/app/portfolio-view/portfolio-view.component.ts
+++ src/app/portfolio-view/portfolio-view.component.ts
@@ -20,7 +20,7 @@ export class PortfolioViewComponent implements OnInit {
 				shortDescription: 'Personal Learning Environment', 
 				description: '', 
 				background: {
-					image: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7) ), url(https://pro2-bar-s3-cdn-cf3.myportfolio.com/84bc6461bb2abbb65ae9e645f8f60b7d/464b0568-94e4-488f-b2a2-fb887e973fbd_rw_1920.jpg?h=1008b1b34b97f91924eb0755587cfaef)',
+					image: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4) ), url(https://pro2-bar-s3-cdn-cf3.myportfolio.com/84bc6461bb2abbb65ae9e645f8f60b7d/464b0568-94e4-488f-b2a2-fb887e973fbd_rw_1920.jpg?h=1008b1b34b97f91924eb0755587cfaef)',
 					position: 'center top',
 					size: 'cover'
 				},
@@ -32,7 +32,7 @@ export class PortfolioViewComponent implements OnInit {
 				shortDescription: 'Company Website', 
 				description: '',
 				background: {
-					image: 'linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8) ), url(https://4.bp.blogspot.com/-Ry_TXQPqKOg/WO3JhNBRciI/AAAAAAAApI4/hJPri1K-Ks0x9tF2vS6VMZdtMU66kIbyQCLcB/s1600/IMG_5382.jpg)',
+					image: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4) ), url(https://4.bp.blogspot.com/-Ry_TXQPqKOg/WO3JhNBRciI/AAAAAAAApI4/hJPri1K-Ks0x9tF2vS6VMZdtMU66kIbyQCLcB/s1600/IMG_5382.jpg)',
 					position: 'center top',
 					size: 'cover'
 				},
@@ -44,7 +44,7 @@ export class PortfolioViewComponent implements OnInit {
 				shortDescription: 'Company Website', 
 				description: '',
 				background: {
-					image: 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9) ), url(http://www.jonesbarber.fi/wp-content/uploads/2016/06/homepagelogo-e1472041489437.png)',
+					image: 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5) ), url(http://www.jonesbarber.fi/wp-content/uploads/2016/06/homepagelogo-e1472041489437.png)',
 					position: 'center',
 					size: 'cover'
 				},
@@ -56,19 +56,7 @@ export class PortfolioViewComponent implements OnInit {
 				shortDescription: 'Gaming software', 
 				description: '',
 				background: {
-					image: 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9) ), url(https://images3.alphacoders.com/816/816720.jpg)',
-					position: 'center',
-					size: 'cover'
-				},
-				status: 'Completed'
-			},
-			{ 
-				label: 'Game development', 
-				name: 'Escape The Room', 
-				shortDescription: 'Web Based Escape Room Game', 
-				description: '',
-				background: {
-					image: 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9) ), url(http://blogs.shu.edu/weekender/files/2016/10/ESCAPE-LOGO-730x445.jpg)',
+					image: 'linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4) ), url(https://images3.alphacoders.com/816/816720.jpg)',
 					position: 'center',
 					size: 'cover'
 				},
@@ -80,7 +68,20 @@ export class PortfolioViewComponent implements OnInit {
 				shortDescription: 'HRM Software', 
 				description: '',
 				background: {
-					image: 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9) ), url(http://www.jonesbarber.fi/wp-content/uploads/2016/06/homepagelogo-e1472041489437.png)',
+					image: 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5) ), url(https://png.pngtree.com/thumb_back/fw800/back_pic/03/69/01/7957b416b366534.jpg)',
+					position: 'center',
+					size: 'cover'
+				},
+				status: 'Completed'
+			},
+
+			{ 
+				label: 'Game development', 
+				name: 'Escape The Room', 
+				shortDescription: 'Web Based Escape Room Game', 
+				description: '',
+				background: {
+					image: 'linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4) ), url(https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png)',
 					position: 'center',
 					size: 'cover'
 				},
