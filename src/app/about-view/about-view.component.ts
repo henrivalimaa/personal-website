b8d7e553fffe535fc1532eebd602f4e37eea35ef
henrivalimaa <valimaa.henri@gmail.com>
2018-10-04 10:01:54 +0300

    Portfolio and Blog view changes

--- src/app/about-view/about-view.component.ts
+++ src/app/about-view/about-view.component.ts
@@ -11,22 +11,40 @@ import { logoFadeAnimation } from "../animation/animations";
   animations: [logoFadeAnimation]
 })
 export class AboutViewComponent implements OnInit {
-  private skills: Array<any>;
+  private skills: any;
+  private view: any;
 
   constructor() { }
 
   ngOnInit() {
-    this.skills = [
-      { label: 'Angular', level: 4 },
-      { label: 'AngularJS', level: 4 },
-      { label: 'CSS/Sass', level: 4 },
-      { label: 'ES6 + TypeScript', level: 3 },
-      { label: 'Java', level: 3, description: 'Spring & Hibernate' },
-      { label: 'Python', level: 3, description: 'Django Frameworks' },
-      { label: 'C# (.NET)', level: 3 },
-      { label: 'React + Redux', level: 2, description: 'Learning in progress' },
-      { label: 'AWS & GCP', level: 3 }
-    ];
+    this.skills = { 
+      programming: [
+        { label: 'Angular', level: 4 },
+        { label: 'AngularJS', level: 4 },
+        { label: 'CSS/Sass', level: 4 },
+        { label: 'ES6 + TypeScript', level: 3 },
+        { label: 'Java', level: 3, description: 'Spring & Hibernate' },
+        { label: 'Python', level: 3, description: 'Django Frameworks' },
+        { label: 'C# (.NET)', level: 3 },
+        { label: 'React + Redux', level: 2, description: 'Learning in progress' }
+      ],
+      environments: [
+        { label: 'Linux', level: 4 , description: 'Currently using Ubuntu 18.04'},
+        { label: 'Windows', level: 3 },
+        { label: 'Mac OS', level: 2 },
+        { label: 'AWS', level: 3 },
+        { label: 'GCP', level: 3 },
+        { label: 'Azure', level: 2 }
+      ],
+      workflow: [
+        { label: 'Agile Scrum', level: 4 },
+        { label: 'Git', level: 3 },
+        { label: 'Docker', level: 2 },
+        { label: 'LXC & LXD', level: 2 },
+        { label: 'Jenkins', level: 2 },
+        { label: 'UML', level: 4, description: 'Design & Architecture' }
+      ]
+    }
   }
 
 }
