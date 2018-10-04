947f95195677b67a4d0328003f9e19ae2bd55672
henrivalimaa <valimaa.henri@gmail.com>
2018-10-04 11:02:51 +0300

    Map added to blog view

--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google: any;
+
 @Component({
   selector: 'app-blog-view',
   templateUrl: './blog-view.component.html',
@@ -10,6 +12,59 @@ export class BlogViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	this.initMap();
+  }
+
+  initMap(): void {
+  	google.charts.load('current', {
+      'packages':['geochart'],
+      'mapsApiKey': 'AIzaSyDCNVdNPxA_SUJ9H4UK3IKeQLnZvP0Wctw'
+    });
+    google.charts.setOnLoadCallback(this.drawRegionsMap);
+        
+  }
+
+  drawRegionsMap(): void {
+  	let data = google.visualization.arrayToDataTable([
+      ['Country'],
+      ['Germany'],
+      ['France'],
+      ['Russia'],
+      ['Finland'],
+      ['Norway'],
+      ['Denmark'],
+      ['Sweden'],
+      ['Estonia'],
+      ['Lithuania'],
+      ['Thailand'],
+      ['Japan'],
+      ['Vietnam'],
+      ['Latvia'],
+      ['Cambodia'],
+      ['Luxemburg'],
+      ['Ireland'],
+      ['Great Britain'],
+      ['Spain'],
+      ['Greece'],
+      ['Belarus'],
+      ['Belgium'],
+      ['Latvia'],
+      ['Bosnia and Herzegovina'],
+      ['Czech Republic'],
+      ['Bulgaria'],
+      ['Poland'],
+      ['Serbia']
+    ]);
+
+    let options = {
+    	backgroundColor: 'transparent',
+  	 	datalessRegionColor: '#444444',
+  	 	defaultColor: '#ce952d'
+    };
+
+    let chart = new google.visualization.GeoChart(document.getElementById('map_div'));
+
+    chart.draw(data, options);
   }
 
 }
