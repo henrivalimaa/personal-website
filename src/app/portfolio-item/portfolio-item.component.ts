24eeec13a07aed7e83ba37199313f511b3229380
henrivalimaa <valimaa.henri@gmail.com>
2018-10-09 19:58:42 +0300

    Project modal added on mobile

--- src/app/portfolio-item/portfolio-item.component.ts
+++ src/app/portfolio-item/portfolio-item.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { logoFadeAnimation } from "../animation/animations";
+
 @Component({
   selector: 'portfolio-item',
   templateUrl: './portfolio-item.component.html',
-  styleUrls: ['./portfolio-item.component.css']
+  styleUrls: ['./portfolio-item.component.css'],
+  animations: [ logoFadeAnimation ]
 })
 export class PortfolioItemComponent implements OnInit {
 
 	@Input() properties: any;
+	private showProject: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displayProject(): void {
+  	this.showProject = !this.showProject;
   }
 
 }
